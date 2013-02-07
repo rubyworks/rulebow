da917b29682e7bd0c5f6f47fc84615eaf842e00c
7rans <transfire@gmail.com>
2013-02-06 21:01:16 -0500

    Fix CLI and document a little better.

--- lib/fire/cli.rb
+++ lib/fire/cli.rb
@@ -3,15 +3,16 @@ require 'fire/session'
 
 module Fire
 
+  # Fire's command line interface.
   #
   class CLI
 
-    #
+    # Fire her up!
     def self.run(argv=ARGV)
       new(argv).run
     end
 
-    #
+    # Fire her up in autorun mode!
     def self.autorun(argv=ARGV)
       new(argv).autorun
     end
@@ -26,33 +27,34 @@ module Fire
       @session ||= Session.new(:watch=>@watch)
     end
 
-    #
+    # Fire her up!
     def run
       args = cli_parse
       session.run(args)
     end
 
-    #
+    # Fire her up in autorun mode!
     def autorun
       args = cli_parse
       session.autorun(args)
     end
 
-    #
+    # Parse command line arguments with just the prettiest
+    # little CLI parser there ever was.
     def cli_parse
       cli @argv,
         "-T" => method(:list_tasks),
         "-w" => method(:watch)
     end
 
-    #
+    # Print out a list of availabe manual triggers.
     def list_tasks
       puts "(#{session.root})"
       puts session.task_sheet
       exit
     end
 
-    #
+    # Set the watch wait period.
     def watch(seconds)
       @watch = seconds 
     end
